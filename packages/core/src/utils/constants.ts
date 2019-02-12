fbde9096a37f9b5c9efb44607df0b73cf1333c09
Bruno Lemos <brunohplemos@gmail.com>
2019-02-12 05:31:56 -0200

    [WIP] Private repo support: getInstallationToken

--- packages/core/src/utils/constants.ts
+++ packages/core/src/utils/constants.ts
@@ -11,3 +11,7 @@ export const MIN_COLUMN_WIDTH = 320
 
 export const API_BASE_URL = 'https://api.devhubapp.com'
 export const GRAPHQL_ENDPOINT = `${API_BASE_URL}/graphql`
+
+export const GITHUB_APP_CANNONICAL_ID = __DEV__
+  ? 'devhub-localhost-app'
+  : 'devhub-app'
